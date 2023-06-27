27223d1dcedb3c586f54825dfd815371bfdbd768
david-auk <david@aukes.com>
2023-06-27 18:07:44 +0200

    laying and testing first function infrastructure

--- main.py
+++ main.py
@@ -3,13 +3,46 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('Media', type=str, help='the Source that will get Downloaded/Processed (YouTube links | Audio files)')
+parser.add_argument('-i', '--instrumental', type=str, help='Give a instrumental')
+parser.add_argument('-V', '--vocal', type=str, help='Give a Vocal')
 
 args = parser.parse_args()
-audio_path = args.Media
+instrumentalPath = args.instrumental
+vocalPath = args.vocal
 
-print(f'Analyzing \'{audio_path}\'')
-songKey, songKeyType = corefunctions.analyzeSongKey(audio_path)
+print('\n')
 
-if songKey is not None:
-	print(f'The key of the song is:', songKey, songKeyType)
+print(f'Analyzing instrumental: \'{instrumentalPath.split("/")[-1]}\'')
+instrumentalKey, instrumentalKeyType = corefunctions.analyzeSongKey(instrumentalPath)
+
+if instrumentalKey is None:
+	quit()
+
+print(f'The key of the instrumental is:', instrumentalKey, instrumentalKeyType)
+
+print()
+
+print(f'Analyzing vocal: \'{vocalPath.split("/")[-1]}\'')
+vocalKey, vocalKeyType = corefunctions.analyzeSongKey(vocalPath)
+
+if instrumentalKey is None:
+	quit()
+
+print(f'The key of the vocal is:', vocalKey, vocalKeyType)
+
+print()
+
+if instrumentalKeyType != vocalKeyType:
+
+	print(f'Minor and Major do not harmonise. resolving Instrumental: {instrumentalKey} {instrumentalKeyType}')
+
+	instrumentalKey, instrumentalKeyType = corefunctions.circleOfFithResolver(key=instrumentalKey, keyType=instrumentalKeyType)
+
+	print(f'The key of the instrumental is:', instrumentalKey, instrumentalKeyType)
+
+print('\n')
+
+shortestSemitoneDistance = corefunctions.distanceCalc(instrumentalKey=instrumentalKey, vocalKey=vocalKey)
+
+print(f'your instrumental instructions are: {shortestSemitoneDistance} steps (semitones), {shortestSemitoneDistance}00 cent in pitch')
+
